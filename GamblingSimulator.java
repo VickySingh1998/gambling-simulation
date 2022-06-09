12b07e410a9947fbf78e318e960fe88c382ed2ce
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-09 22:13:47 +0530

    added: condition to leave the game for the day

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,26 +2,29 @@ public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
         System.out.println("     ");
-        int stake=100;
-        int bet=1;
+        int stake = 100;
+        int bet = 1;
+        int totalAmount=0;
 
-        while (stake>50 && stake<150) {
+        while (stake > 50 && stake < 150) {
             int bettingAmount = (int) Math.floor(Math.random() * 10) % 2;
 
             if (bet == bettingAmount) {
                 System.out.println("It's a win");
-                stake = stake + bet;
-                System.out.println("Your new stake amount is: " +stake);
+                totalAmount = stake + bet;
             } else {
                 System.out.println("So sorry you lose");
-                stake = stake - bet;
-                System.out.println("Your new stake amount is: " + stake);
+                totalAmount = stake - bet;
             }
+            System.out.println("New Stake Amount: " + totalAmount);
+            stake = totalAmount;
         }
-            System.out.println("Your Total Amount Now: " + stake);
-        if (stake == 50 || stake==150){
-            System.out.println("             ");
-            System.out.println("I would like to resign for the day");
+        System.out.println(" ");
+        System.out.println("Your Total Amount Now: " + stake);
+
+        if (stake == 50 || stake == 150) {
+            System.out.println(" ");
+            System.out.println("Player would like to resign for the today");
         }
     }
 }
