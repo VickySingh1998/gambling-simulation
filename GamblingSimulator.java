d931285be9a6c75c3330310e11f0b3a863098042
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-09 09:26:58 +0530

    added: gambling simulation program

---
+++ GamblingSimulator.java
@@ -0,0 +1,10 @@
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator");
+        int stake=100;
+        int bet=1;
+
+        System.out.println("Your stake amount= "+stake);
+        System.out.println("Your bet amount= "+bet);
+    }
+}
