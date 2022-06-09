bd09460fa35ba7ed2ce03969ea4437f7098e9460
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-09 10:48:34 +0530

    modified: to find win or lose

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,10 +1,20 @@
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
+        System.out.println("     ");
         int stake=100;
         int bet=1;
-
-        System.out.println("Your stake amount= "+stake);
-        System.out.println("Your bet amount= "+bet);
+        int totalAmount;
+        int bettingAmount= (int) Math.floor(Math.random()*10) % 2;
+        
+        if (bet == bettingAmount) {
+            System.out.println("It's a win");
+            totalAmount = stake + bet;
+        }
+        else{
+            System.out.println("So sorry you lose");
+            totalAmount = stake - bet;
+        }
+        System.out.println("Your Total Amount Now: " +totalAmount);
     }
 }
